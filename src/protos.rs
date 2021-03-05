b8fd72205b13e2f8396955e6e558594badee0b6b
Alex P <alexeek@protonmail.com>
2021-03-05 23:48:37 +0000

    refactor: move From impls to protos mod

--- src/protos.rs
+++ src/protos.rs
@@ -1,7 +1,44 @@
+use models::User;
+
 pub mod models {
   tonic::include_proto!("models");
 }
 
 pub mod rest_client {
   tonic::include_proto!("rest_client");
+}
+
+// TODO: Possibly implement a macro to make this cleaner.
+impl From<twilight_model::user::CurrentUser> for User {
+  fn from(u: twilight_model::user::CurrentUser) -> Self {
+    Self {
+      avatar: u.avatar.unwrap_or_default(),
+      bot: u.bot,
+      discriminator: u.discriminator,
+      email: u.email.unwrap_or_default(),
+      id: u.id.0,
+      locale: u.locale.unwrap_or_default(),
+      mfa_enabled: u.mfa_enabled,
+      name: u.name,
+      system: false,
+      verified: u.verified.unwrap_or_default(),
+    }
+  }
+}
+
+impl From<twilight_model::user::User> for User {
+  fn from(u: twilight_model::user::User) -> Self {
+    Self {
+      avatar: u.avatar.unwrap_or_default(),
+      bot: u.bot,
+      discriminator: u.discriminator,
+      email: u.email.unwrap_or_default(),
+      id: u.id.0,
+      locale: u.locale.unwrap_or_default(),
+      mfa_enabled: u.mfa_enabled.unwrap_or_default(),
+      name: u.name,
+      system: u.system.unwrap_or_default(),
+      verified: u.verified.unwrap_or_default(),
+    }
+  }
 }
--- src/rest.rs
+++ src/rest.rs
@@ -6,41 +6,6 @@ use crate::protos::models::User;
 use crate::protos::rest_client::rest_client_server::RestClient as RestClientDefinition;
 use crate::protos::rest_client::{BootstrapRequest, EntityRequest};
 
-// TODO: Possibly implement a macro to make this cleaner.
-impl From<twilight_model::user::CurrentUser> for User {
-  fn from(u: twilight_model::user::CurrentUser) -> Self {
-    Self {
-      avatar: u.avatar.unwrap_or_default(),
-      bot: u.bot,
-      discriminator: u.discriminator,
-      email: u.email.unwrap_or_default(),
-      id: u.id.0,
-      locale: u.locale.unwrap_or_default(),
-      mfa_enabled: u.mfa_enabled,
-      name: u.name,
-      system: false,
-      verified: u.verified.unwrap_or_default(),
-    }
-  }
-}
-
-impl From<twilight_model::user::User> for User {
-  fn from(u: twilight_model::user::User) -> Self {
-    Self {
-      avatar: u.avatar.unwrap_or_default(),
-      bot: u.bot,
-      discriminator: u.discriminator,
-      email: u.email.unwrap_or_default(),
-      id: u.id.0,
-      locale: u.locale.unwrap_or_default(),
-      mfa_enabled: u.mfa_enabled.unwrap_or_default(),
-      name: u.name,
-      system: u.system.unwrap_or_default(),
-      verified: u.verified.unwrap_or_default(),
-    }
-  }
-}
-
 #[derive(Default)]
 pub struct Client {
   client: Mutex<Option<twilight_http::Client>>,
