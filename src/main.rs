ec65b302ebd42d638320575620d14205cf6d999f
Alex P <alexeek@protonmail.com>
2021-03-05 23:23:43 +0000

    feat: add entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::transport::Server;
+
+mod rest;
+use rest::{Client as RestClient, Server as RestClientServer};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt().init();
+
+    let addr = std::env::var("SERVER_ADDR")?.parse()?;
+    tracing::info!(message = "Starting server.", %addr);
+
+    Server::builder()
+        .trace_fn(|_| tracing::info_span!("scaffolding"))
+        .add_service(RestClientServer::new(RestClient::default()))
+        .serve(addr)
+        .await?;
+    Ok(())
 }
