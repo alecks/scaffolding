df4a1d580f9ed6270615a002a2ccbd1efe2f8c72
Alex P <alexeek@protonmail.com>
2021-03-06 01:40:52 +0000

    feat(#2): implement From trait for channels and permissions

--- src/protos.rs
+++ src/protos.rs
@@ -1,4 +1,5 @@
-use models::User;
+use models::{PermissionOverwrite, TextChannel, User};
+use twilight_model::channel::permission_overwrite;
 
 pub mod models {
   tonic::include_proto!("models");
@@ -39,6 +40,43 @@ impl From<twilight_model::user::User> for User {
       name: u.name,
       system: u.system.unwrap_or_default(),
       verified: u.verified.unwrap_or_default(),
+    }
+  }
+}
+
+impl From<twilight_model::channel::TextChannel> for TextChannel {
+  fn from(c: twilight_model::channel::TextChannel) -> Self {
+    Self {
+      guild_id: c.guild_id.unwrap_or_default().0,
+      id: c.id.0,
+      last_message_id: c.last_message_id.unwrap_or_default().0,
+      last_pin_timestamp: c.last_pin_timestamp.unwrap_or_default(),
+      name: c.name,
+      nsfw: c.nsfw,
+      parent_id: c.parent_id.unwrap_or_default().0,
+      permission_overwrites: c
+        .permission_overwrites
+        .into_iter()
+        .map(|x| x.into())
+        .collect(),
+      position: c.position,
+      rate_limit_per_user: c.rate_limit_per_user.unwrap_or_default(),
+      topic: c.topic.unwrap_or_default(),
+    }
+  }
+}
+
+impl From<permission_overwrite::PermissionOverwrite> for PermissionOverwrite {
+  fn from(p: permission_overwrite::PermissionOverwrite) -> Self {
+    let (id, kind) = match p.kind {
+      permission_overwrite::PermissionOverwriteType::Member(id) => (id.0, 0),
+      permission_overwrite::PermissionOverwriteType::Role(id) => (id.0, 1),
+    };
+    Self {
+      allow: p.allow.bits(),
+      deny: p.deny.bits(),
+      id: id,
+      kind: kind,
     }
   }
 }
